27f9915e1a00c0516c6feb3aa5191301bf8bb1da
Amalie <amhe0001@stud.kea.dk>
2024-02-21 13:05:34 +0100

    blandet

--- raavarer.js
+++ raavarer.js
@@ -1,57 +1,34 @@
-const urlParams = new URLSearchParams(window.location.search);
-const category = urlParams.get("category");
-
-//Fetche
-fetch(`https://tamnyujkkpzvesfbwiez.supabase.co/rest/v1/R%C3%A5varer?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhbW55dWpra3B6dmVzZmJ3aWV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgzNTQ2MjMsImV4cCI6MjAyMzkzMDYyM30.SY1vHy1FhIiLDJ-nvGmRIHWWw-K6Q6hfvzFxKi3MahU=` + category)
+fetch("https://tamnyujkkpzvesfbwiez.supabase.co/rest/v1/R%C3%A5varer?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhbW55dWpra3B6dmVzZmJ3aWV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgzNTQ2MjMsImV4cCI6MjAyMzkzMDYyM30.SY1vHy1FhIiLDJ-nvGmRIHWWw-K6Q6hfvzFxKi3MahU")
   .then((res) => res.json())
-  .then(showProdukter);
+  .then(showFoods);
 
-function showProdukter(products) {
-  //loope - brug forEach
-  products.forEach(showProdukter);
+function showFoods(foods) {
+  console.log("foods", foods);
+  //Looper og viser 1 food
+  foods.forEach(showFood);
 }
 
-//Trækker i templaten fra HTML
-produkterTemplate = document.querySelector(".varer_template");
-console.log("varer template", produkterTemplate);
-
-//Trækker i containeren fra HTML - forældreelement
-produkterContainer = document.querySelector(".varer_container");
-console.log("varer container", produkterContainer);
-
-function showProdukter(produkterJSON) {
-  let produkterClone;
-  //looper
-  produkterJSON.forEach((produkter) => {
-    console.log("Produkter", produkter);
-    //Kloner til templaten.
-    produkterClone = produkterTemplate.cloneNode(true).content;
-    //textContent virker IKKE på billeder --> brug src.
-    produkterClone.querySelector(".image_container").src = ` "https://vildmadv2.vps.webdock.io/application/files/4316/2436/8826/${produkter.id}.png`;
-    produkterClone.querySelector(".image_container").alt = `Picture of ${produkter.name}`;
-    //Overskriften får en tekst udskrevet, der er lig med vores "forEach + JSON-kategorinavn (find navn i JSON)"
-    produkterClone.querySelector(".varer_overskrift").textContent = produkter.productdisplayname;
-    // produkterClone.querySelector(".varer_sub_overskrift").textContent = `${produkter.articletype} || ${produkter.brandname}`;
-    produkterClone.querySelector(".price_normal").textContent = "DKK " + produkter.price + ",-";
-    produkterClone.querySelector("a").setAttribute("href", `produkt.html?id=${produkter.id}`);
-    produkterClone.querySelector(".sale-tag").textContent = `${produkter.discount} %`;
-
-    //UDSOLGT SKILT
-    // if (produkter.soldout) {
-    //   produkterClone.querySelector(".udsolgt-tag").classList.remove("skjul");
-    //   produkterClone.querySelector(".image_container").classList.add("udsolgt");
-    // }
-
-    //UDSALG
-    // if (produkter.discount) {
-    //   produkterClone.querySelector(".sale-tag").classList.remove("skjul");
-    //   produkterClone.querySelector(".price_normal").classList.add("skjul");
-    //   produkterClone.querySelector(".price_before").classList.remove("skjul");
-    //   produkterClone.querySelector(".price_before").textContent = "DKK " + produkter.price + " ,-";
-    //   produkterClone.querySelector(".price_sale").classList.remove("skjul");
-    //   produkterClone.querySelector(".price_sale").textContent = "DKK " + (produkter.price * (100 - produkter.discount)) / 100 + " ,-";
-    // }
-    //Børneelement til produkter_container. Det betyder at tingene vil ændre sig i containeren.
-    produkterContainer.appendChild(produkterClone);
-  });
+function showFood(food) {
+  console.log("madder");
+  //Går i HTML og fanger templaten
+  const template = document.querySelector("#foodTemplate").content;
+  const copy = template.cloneNode(true);
+  copy.querySelector(".h3_overskrift").textContent = food.name;
+  copy.querySelector(".h4_overskrift").textContent = food.catgories;
+  //Billedet skifter ikke fordi linket driller
+  // document.querySelector(".main_img").src = `https://vildmadv2.vps.webdock.io/application/files/4316/2436/${food.id}.png`;
+  document.querySelector("main").appendChild(copy);
 }
+
+// {
+//   "id": 1,
+//   "created_at": "2024-02-19T15:12:01+00:00",
+//   "name": "Watercress",
+//   "sankested": "Watercress can be found in and around streams, lakes, and creeks that have a good flow of clear water. It can grow on the banks or in the water itself (though in that case, part of the plant will be hidden). Take care not to pick watercress in streams where cattle or sheep graze nearby, as infected animals can transmit a liver fluke parasite to humans.\nLakes, streams.",
+//   "season": "You can normally pick watercress from May until November, but in milder winters you might be lucky enough to find it growing year round.\nEntire plant: May, June, July, August, September, October, November.",
+//   "beskrivelse": "Watercress can grow 40-60 cm high and has hollow, succulent stems. The leaves are oval-shaped and look like small water lilies. They grow in pairs on side shoots off the main stem and end in a single round leaf. Smooth all over, the plant develops small, white flowers that are yellowish-green in the center.",
+//   "sankning": "Cut or snip the young shoots off the plant. When watercress blossoms, its sharpness intensifies, so unless you can't get enough of that flavor, avoid gathering it then.",
+//   "img": "https://vildmadv2.vps.webdock.io/application/files/4316/2436/8826/Brondkarse_ravarkort_app.png",
+//   "catgories": "herbs and small growths",
+//   "top_beskrivelse": "Watercress is a sharp-tasting herb that grows abundantly alongside streams and creeks. It has been used in cooking for centuries; watercress soup, for example, is a classic in many cuisines. "
+// }
