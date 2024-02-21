7bf1b69c4571a8798197caa56451829c973880d1
Anika <anso0004@stud.kea.dk>
2024-02-21 15:59:42 +0100

    Færdige resultat

--- raavarer.js
+++ raavarer.js
@@ -28,17 +28,3 @@ function showFood(food) {
   // document.querySelector(".main_img").src = `https://vildmadv2.vps.webdock.io/application/files/4316/2436/${food.id}.png`;
   document.querySelector(".beer_container").appendChild(copy);
 }
-
-//food.img
-// {
-//   "id": 1,
-//   "created_at": "2024-02-19T15:12:01+00:00",
-//   "name": "Watercress",
-//   "sankested": "Watercress can be found in and around streams, lakes, and creeks that have a good flow of clear water. It can grow on the banks or in the water itself (though in that case, part of the plant will be hidden). Take care not to pick watercress in streams where cattle or sheep graze nearby, as infected animals can transmit a liver fluke parasite to humans.\nLakes, streams.",
-//   "season": "You can normally pick watercress from May until November, but in milder winters you might be lucky enough to find it growing year round.\nEntire plant: May, June, July, August, September, October, November.",
-//   "beskrivelse": "Watercress can grow 40-60 cm high and has hollow, succulent stems. The leaves are oval-shaped and look like small water lilies. They grow in pairs on side shoots off the main stem and end in a single round leaf. Smooth all over, the plant develops small, white flowers that are yellowish-green in the center.",
-//   "sankning": "Cut or snip the young shoots off the plant. When watercress blossoms, its sharpness intensifies, so unless you can't get enough of that flavor, avoid gathering it then.",
-//   "img": "https://vildmadv2.vps.webdock.io/application/files/4316/2436/8826/Brondkarse_ravarkort_app.png",
-//   "catgories": "herbs and small growths",
-//   "top_beskrivelse": "Watercress is a sharp-tasting herb that grows abundantly alongside streams and creeks. It has been used in cooking for centuries; watercress soup, for example, is a classic in many cuisines. "
-// }
